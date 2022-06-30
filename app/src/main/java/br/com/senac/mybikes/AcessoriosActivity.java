6800f9cf74571b39f02e2beb56faeb6a21dc6d3f
maikcarvalho <maikdantas2@gmail.com>
2022-06-30 17:18:32 -0300

    botoes

---
+++ app/src/main/java/br/com/senac/mybikes/AcessoriosActivity.java
@@ -0,0 +1,14 @@
+package br.com.senac.mybikes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcessoriosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acessorios_layout);
+    }
+}
